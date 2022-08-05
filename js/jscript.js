c74ccdae60669c0e84002d3a45e8c9e66c7a7d89
kopildas <102140363+kopildas@users.noreply.github.com>
2022-08-06 04:04:09 +0600

    almost doooone

--- js/jscript.js
+++ js/jscript.js
@@ -61,6 +61,21 @@ $(document).ready(function () {
         }
     });
 
+     // typing text animation script
+     var typed = new Typed(".typing", {
+        strings: ["UI/UX Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
 
 
     // Parallax
