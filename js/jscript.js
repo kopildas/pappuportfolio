2b46f22ef36df5bfe495da4977cf6d042d8c3476
kopildas <102140363+kopildas@users.noreply.github.com>
2022-08-11 00:06:23 +0600

    project section nd some bugs fixed

--- js/jscript.js
+++ js/jscript.js
@@ -143,9 +143,29 @@ function opentab(tabname){
 //$("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
 function togglePopup(){
     document.getElementById("popup-1").classList.toggle("active");
+    // document.getElementById("popup-1").classList.toggle("active");
     document.addEventListener('keydown', function(event){
         if(event.key === "Escape"){
     document.getElementById("popup-1").classList.toggle("active");
+        }
+    });
+}
+
+function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+    // document.getElementById("popup-1").classList.toggle("active");
+    document.addEventListener('keydown', function(event){
+        if(event.key === "Escape"){
+    document.getElementById("popup-2").classList.toggle("active");
+        }
+    });
+}
+function togglePopup3(){
+    document.getElementById("popup-3").classList.toggle("active");
+    // document.getElementById("popup-1").classList.toggle("active");
+    document.addEventListener('keydown', function(event){
+        if(event.key === "Escape"){
+    document.getElementById("popup-3").classList.toggle("active");
         }
     });
 }
